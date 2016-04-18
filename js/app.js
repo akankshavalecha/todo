72fdd4671235c6567b7a29fdcc374a0ddf139b05
Akanksha Valecha <akanksha.valecha@gmail.com>
2016-04-18 07:34:04 +0530

    added toasters

--- js/app.js
+++ js/app.js
@@ -14,33 +14,42 @@ app = angular.module('todo', ['ngDragDrop','toaster']);
 			    };
 			});
 
-			app.controller('todoController', function($scope) {
+			app.controller('todoController',['$rootScope', '$scope', 'toaster', function($rootScope,$scope, toaster) { 
 				window.x = $scope;
 				$scope.task = '';
-				$scope.inputcolor = 'bgwhite';
+				$scope.inputcolor = '';
+				$scope.doneListItem = {};
 				$scope.doneList = [];
 				var colorsClasses = ['bgblue','bggreen', 'bgred', 'bgteal', 
 									'bgorange', 'bgyellow', 'bggray', 'bgwhite']; 
-				$scope.todoList = [
+				var todoList = [
 				{text:'learn angular', done:true, card: 'bgwhite', 'drag': true},
 				{text:'build an angular app', done:false, card: 'bgwhite', 'drag': true},
 				{text:'work', done:true, card: 'bgorange', 'drag': true},
-				{text:'gym', done:false, card: 'bggreen', 'drag': true},
+				{text:'gym', done:false, card: 'bggreen', 'drag': true}
 				];
+				$scope.saved = angular.fromJson(localStorage.getItem('todoList'));
+				$scope.todoList = (localStorage.getItem('todoList'))!==null ? $scope.saved : todoList;
+				localStorage.setItem('todoList', angular.toJson($scope.todoList));
 
 				$scope.addTodo = function() {
 					if($scope.task.length){
-						$scope.inputcolor = colorsClasses[Math.floor((Math.random() * colorsClasses.length))]
+						$scope.inputcolor = ($scope.inputcolor.length) ? $scope.inputcolor : colorsClasses[Math.floor((Math.random() * colorsClasses.length))]
 						$scope.todoList.push({text:$scope.task, done:false, card: $scope.inputcolor, 'drag': true});
 						$scope.task = '';
-
-						$scope.inputcolor = 'bgwhite';
+						$scope.inputcolor = '';
+						localStorage.setItem('todoList', angular.toJson($scope.todoList));
 					}
 				};
 				$scope.removeTodo = function(list, todo){
 					var index = list.indexOf(todo);
 					if(index > -1){
 						list.splice(index,1);
+					}
+					if(list === 'todoList'){
+						localStorage.setItem('todoList', angular.toJson($scope.todoList));
+					}else if(list === 'doneList'){
+						localStorage.setItem('doneList', angular.toJson($scope.doneList));
 					}
 					return list;
 				}
@@ -53,21 +62,23 @@ app = angular.module('todo', ['ngDragDrop','toaster']);
 					return count;
 				};
 
-				$scope.archive = function() {
-					var oldTodos = $scope.todos;
-					$scope.todos = [];
-					angular.forEach(oldTodos, function(todo) {
-						if (!todo.done) $scope.todos.push(todo);
-					});
-				};
+				
 
 				$scope.notify=function(event,ui){
-					console.log('kkk')
-        // toaster.pop('success', "BlogBeats", ui.draggable[0].innerHTML+" is dropped into "+event.target.id);
+					$scope.doneList.push($scope.doneListItem);
+					// console.log(ui.draggable[0].index)
+					localStorage.setItem('todoList', angular.toJson($scope.todoList));
+					localStorage.setItem('doneList', angular.toJson($scope.doneList));
+					if(event.target.id === 'TodoList'){
+						var type = 'error';
+					}else if(event.target.id === 'DoneList'){
+						var type = 'success';
+					}else{var type = 'warning'; }
+			        toaster.pop(type, "To-Do", "A Task is dropped into "+ event.target.id);
         
-      };
+      			};
 
 
 				
 
-			});
+			}]);
